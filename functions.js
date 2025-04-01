6fc45bdf9254a455ad6cad7a6dfe91dc62eb6db7
Ani Argjiri <aargjiri@akamai.com>
2025-04-01 11:24:56 +0200

    fix: minor fix backportable to v5.7

--- functions.js
+++ functions.js
@@ -3,7 +3,7 @@
 // a new feature
 // added a small fix for the new feature
 // yet a new feature
-// minor fix
+// minor fix | minor fix backportable to v5.7
 
 // another new feature
 // a fix for the new feature
